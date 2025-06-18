5fdf545518d7bba846f6773fc3b0e72bad2da29b
morisummer <timofeevnikita111@gmail.com>
2025-06-18 21:52:46 +0300

    Reformat

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import {Button} from "@/components/ui/button";
+	import { Button } from '@/components/ui/button';
 
-  let cnt = $state(0);
+	let cnt = $state(0);
 </script>
 
 <h1>Hello world!</h1>
-<Button
-  onclick={(() => cnt++)}
->Click me ! ({cnt})
-</Button>
+<Button onclick={() => cnt++}>Click me ! ({cnt})</Button>
