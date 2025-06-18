3aeeee4a3e26dd71dddd04ed339b7b2b658cbc5f
morisummer <timofeevnikita111@gmail.com>
2025-06-18 21:52:00 +0300

    Add login and register pages

---
+++ frontend/src/routes/auth/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import projector_logo from "$lib/assets/projector_logo.jpg";
+
+  let {children} = $props();
+</script>
+<div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+  <div class="flex w-full max-w-sm flex-col gap-6">
+    <a href="/" class="flex items-center gap-2 self-center font-medium">
+      <div
+        class="flex size-12 items-center justify-center"
+      >
+        <img src={projector_logo} alt="ProjectOR Logo" class="size-12 rounded-md"/>
+      </div>
+      ProjectOR
+    </a>
+    {@render children()}
+  </div>
+</div>
---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import {Button} from "$lib/components/ui/button/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import {Label} from "$lib/components/ui/label/index.js";
+  import {Input} from "$lib/components/ui/input/index.js";
+
+  const id = $props.id();
+
+  let {email, password} = $state({
+    email: "",
+    password: ""
+  });
+</script>
+
+<div class="flex flex-col gap-6">
+  <Card.Root>
+    <Card.Header class="text-center">
+      <Card.Title class="text-xl">Welcome back</Card.Title>
+      <Card.Description>
+        Sign in to your account to continue collaborating!
+      </Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <form>
+        <div class="grid gap-6">
+          <div class="grid gap-6">
+            <div class="grid gap-3">
+              <Label for="email-{id}">Email</Label>
+              <Input
+                bind:value={email}
+                id="email-{id}"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div class="grid gap-3">
+              <Label for="password-{id}">Password</Label>
+              <Input bind:value={password} id="password-{id}" type="password" required/>
+            </div>
+            <Button
+              type="submit" class="w-full"
+              disabled={!email || !password}
+            >Login
+            </Button>
+          </div>
+          <div class="text-center text-sm">
+            Don&apos;t have an account?
+            <a href="/auth/register" class="underline underline-offset-4"> Sign up </a>
+          </div>
+        </div>
+      </form>
+    </Card.Content>
+  </Card.Root>
+  <div
+    class="text-muted-foreground *:[a]:hover:text-primary *:[a]:underline *:[a]:underline-offset-4 text-balance text-center text-xs"
+  >
+    By clicking continue, you agree to our <a href="##">Terms of Service</a>
+    and <a href="##">Privacy Policy</a>.
+  </div>
+</div>
