dff92613a38263a0079be6f971964c25c6d439c1
Andukov Almaz <almazandukov7@gmail.com>
2025-06-24 23:08:47 +0300

    ✨ Update DATABASE_URL configuration to use environment variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,6 @@
 from os import getenv
 
-# DATABASE_URL = (
-#     f"postgresql+asyncpg://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@"
-#     f"{getenv('POSTGRES_HOST')}:{getenv('POSTGRES_PORT')}/{getenv('POSTGRES_DB')}"
-# )
-
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/projector"
+DATABASE_URL = (
+    f"postgresql+asyncpg://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@"
+    f"{getenv('POSTGRES_HOST')}:{getenv('POSTGRES_PORT')}/{getenv('POSTGRES_DB')}"
+)
