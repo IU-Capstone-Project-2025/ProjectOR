134f0ad8319d410d2d092e2ac3445399c3c20dfb
Thi1ef <kve100878@gmail.com>
2025-07-17 14:39:01 +0300

    test fix

--- backend/app/routes/api/project/__tests__/test_router.py
+++ backend/app/routes/api/project/__tests__/test_router.py
@@ -26,7 +26,8 @@ def sample_projects():
         ProjectSchema(
             id=1,
             title="Project 1",
-            description="Description 1",
+            brief_description="Description 1",
+            description=None,
             is_public=True,
             is_dead=False,
             is_opensource=True,
@@ -36,7 +37,8 @@ def sample_projects():
         ProjectSchema(
             id=2,
             title="Project 2",
-            description="Description 2",
+            brief_description="Description 2",
+            description=None,
             is_public=True,
             is_dead=False,
             is_opensource=True,
